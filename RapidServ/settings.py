aaedb8eeec7225507b20e791ba85830129ee0884
joaomielli <mielli@alunos.utfpr.edu.br>
2023-09-05 15:29:48 -0300

    troca do banco de dados de postgresSQL para sqlite

--- RapidServ/settings.py
+++ RapidServ/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'RapidServ.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rapidserv',
-        'USER': 'postgres',
-        'PASSWORD': 'b1k3',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
